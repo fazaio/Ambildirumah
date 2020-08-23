0da303554cf6d62e6e1c76d44d4f28cf1706de52
fazaio <faza6028@gmail.com>
2020-08-23 19:53:41 +0700

    add login page

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -2,10 +2,25 @@
   <div class="container">
     <div class="row">
       <div>animasi</div>
-      <div class="form">
-        <h3 class="orange">Selamat datang</h3>
-        <span>Silahkan <b>login</b> terlebih dahulu sebelum melanjutkan!</span>
-        <input type="text" placeholder="+62" />
+      <div>
+        <div class="form">
+          <h3 class="orange">Selamat datang</h3>
+          <span>
+            Silahkan login menggunakan
+            <br />nomor <b style="color: #4ac959;">Whatsapp</b> terlebih dahulu
+            sebelum melanjutkan!
+          </span>
+          <input type="text" placeholder="+62" />
+          <div class="left">
+            <button class="btn">masuk</button>
+          </div>
+        </div>
+        <div class="center">
+          Belum punya akun?
+          <span class="orange">
+            <nuxt-link to="/">Register disini.</nuxt-link>
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -22,19 +37,32 @@ export default {}
 }
 .form {
   text-align: left;
+  padding: 20px 10px;
+  border-radius: 5px;
+  /* background-color: #333; */
+  box-shadow: 0 10px 40px -10px rgba(0, 64, 128, 0.2);
 }
 .form > span {
   font-size: 12px;
   display: block;
-  padding: 5px opx;
+  padding: 5px 0px;
   margin: 5px 0px;
   color: #666;
   margin-bottom: 30px;
+  line-height: 18px;
 }
-input {
-  width: 100%;
-  padding: 10px;
+.left {
+  text-align: right;
+  margin-top: 10px;
+}
+.center {
+  margin-top: 10px;
+  text-align: center;
+  font-size: 12px;
+  color: #666;
+}
+.btn {
+  padding: 8px 10px;
   border-radius: 5px;
-  border: 1px solid #ccc;
 }
 </style>
